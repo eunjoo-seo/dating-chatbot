72f6d0d1d45df1117ebf7ee0a91c3cf9efee0844
Eunjoo <seo.e.j29@gmail.com>
2026-02-18 14:17:25 -0500

    Delete john-profile-nav.js

--- john-profile-nav.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const profileNavButton = document.querySelector('.profile-nav-button');
-
-    if (profileNavButton) {
-        profileNavButton.addEventListener('click', () => {
-            window.location.href = 'john-profile.html';
-        });
-    }
-});
